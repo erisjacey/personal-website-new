ae83d49927e7d2e65adfad37bef370c2784f0368
erisjacey-MACBOOK <eris_jacey@hotmail.com>
2022-05-02 18:18:29 +0800

    Install mui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {
+  ThemeProvider, createTheme, responsiveFontSizes,
+} from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+import 'src/App.css';
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
+theme = responsiveFontSizes(theme);
 
 const App = () => (
-  <h1>
-    Hello World from Eris Jacey :)
-  </h1>
+  <ThemeProvider theme={theme}>
+    <h1>
+      Hello World from Eris Jacey :)
+    </h1>
+  </ThemeProvider>
 );
 
 export default App;

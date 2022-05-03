6935b7ca1169880f7b1772312acacaed9e3c60c6
erisjacey-DESKTOP <eris_jacey@hotmail.com>
2022-05-03 16:30:08 +0800

    Init experience assets

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,15 @@
+// Links
+
 const LINK_EMAIL = 'mailto:eris_jacey@hotmail.com';
 const LINK_GITHUB = 'https://github.com/erisjacey';
 const LINK_LINKEDIN = 'https://www.linkedin.com/in/eris-jacey-masagca-309795197/';
+
+// Websites
+const WEBSITE_MATCHUB = 'https://matchub.co/';
 
 export {
   LINK_EMAIL,
   LINK_GITHUB,
   LINK_LINKEDIN,
+  WEBSITE_MATCHUB,
 };
--- src/pages/experience/experience.js
+++ src/pages/experience/experience.js
@@ -1,5 +1,70 @@
 import React from 'react';
 import { Container, Box, Typography } from '@mui/material';
+import LogoNUS from 'myAssets/experience/logo_nus.png';
+import LogoACJC from 'myAssets/experience/logo_acjc.png';
+import LogoMatchub from 'myAssets/experience/logo_matchub.png';
+import TranscriptNUS from 'myAssets/experience/transcript_nus.pdf';
+import TranscriptACJC from 'myAssets/experience/transcript_acjc.pdf';
+import TeachingFeedbackReports from 'myAssets/experience/teaching-feedback-reports.pdf';
+import { WEBSITE_MATCHUB } from 'myConstants';
+
+const AcademicExperience = [
+  {
+    title: 'National University of Singapore',
+    logo: LogoNUS,
+    description: 'B. Comp Computer Science (Hons)',
+    duration: '2019 - 2023',
+    grade: 'Current CAP: 4.73 / 5.0',
+    transcript: TranscriptNUS,
+  },
+  {
+    title: 'Anglo-Chinese Junior College',
+    logo: LogoACJC,
+    description: 'GCE A Levels',
+    duration: '2015 - 2016',
+    grade: '5 Distinctions',
+    transcript: TranscriptACJC,
+  },
+];
+
+const WorkExperience = [
+  {
+    title: 'MatcHub.co',
+    logo: LogoMatchub,
+    role: 'Software Engineer Intern',
+    duration: 'Dec 2020 - Aug 2021',
+    modules: [
+      'CP3200',
+    ],
+    skills: [
+      'Full-Stack', 'React.js', 'Django', 'AWS',
+      'Python', 'JavaScript', 'Netlify', 'Docker',
+    ],
+    link: {
+      name: 'Visit Website',
+      url: WEBSITE_MATCHUB,
+    },
+  },
+  {
+    title: 'National University of Singapore',
+    logo: LogoNUS,
+    role: 'Teaching Assistant',
+    duration: 'Aug 2020 - Apr 2022',
+    modules: [
+      'CS1010E',
+      'CS1101S',
+      'CS2030S',
+    ],
+    skills: [
+      'Java', 'JavaScript', 'Python', 'Vim', 'Functional Programming',
+      'Object-Oriented Programming', 'Procedural Programming',
+    ],
+    link: {
+      name: 'View Teaching Feedback Reports',
+      url: TeachingFeedbackReports,
+    },
+  },
+];
 
 const Experience = () => (
   <Container id="experience">

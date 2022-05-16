c54d1011233310eef560ca195bcf58c7e9474736
erisjacey-DESKTOP <eris_jacey@hotmail.com>
2022-05-16 17:50:02 +0800

    Contact modal frontend

--- src/components/contactModal/contactModal.js
+++ src/components/contactModal/contactModal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {
   Typography, Fade, Box, Modal, Backdrop, Stack,
+  TextField, Button,
 } from '@mui/material';
 
 const style = {
@@ -11,8 +12,9 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: {
     xs: '90%',
-    md: '70%',
-    lg: '50%',
+    md: '65%',
+    lg: '55%',
+    xl: '40%',
   },
   bgcolor: 'background.paper',
   border: '2px solid #000',
@@ -39,12 +41,35 @@ const ContactModal = ({ open, handleClose }) => (
           direction="column"
           justifyContent="center"
           alignItems="stretch"
-          spacing={1}
+          spacing={1.5}
         >
           <Typography id="transition-modal-title" variant="h3">
-            Title
+            Contact Me
           </Typography>
+          <Stack
+            direction="row"
+            justifyContent="space-around"
+            alignItems="center"
+            spacing={1.5}
+            sx={{ '& .MuiTextField-root': { width: '100%' } }}
+          >
+            <TextField required id="first-name" label="First Name" variant="outlined" size="small" />
+            <TextField id="last-name" label="Last Name" variant="outlined" size="small" />
+          </Stack>
+          <TextField required id="email" label="Email" variant="outlined" size="small" />
+          <TextField id="subject" label="Subject" variant="outlined" size="small" />
+          <TextField
+            required
+            id="message"
+            label="Message"
+            variant="outlined"
+            multiline
+            rows={10}
+          />
         </Stack>
+        <Button variant="outlined" onClick={handleClose} sx={{ marginTop: '2%' }}>
+          Send
+        </Button>
       </Box>
     </Fade>
   </Modal>
